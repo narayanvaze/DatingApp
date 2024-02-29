e46802e61e55c00308300d81dc26c35fcbb55fa5
narayanvaze <vazenarayan13@gmail.com>
2024-02-29 17:41:06 +0530

    End of Section 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Member } from '../_models/member';
+import { map, of } from 'rxjs';
 
 
 @Injectable({
@@ -9,19 +10,32 @@ import { Member } from '../_models/member';
 
 export class MembersService {
   baseUrl = 'https://localhost:5001/api/';
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    console.log("In getMembers()");
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if(this.members.length > 0) return of (this.members);
+
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => { 
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(username: string){
+    const member = this.members.find(x => x.userName === username);
+    if(member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/'+ username);
   }
 
   updateMember(member: Member){
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+      })
+    );
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,8 @@ import { NotFoundComponent } from './_errors/not-found/not-found.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_intercepts/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_intercepts/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -46,11 +48,13 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     BrowserAnimationsModule,
     FormsModule,
     NgbModule,
-    SharedModule
+    SharedModule,
+    NgxSpinnerModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true} 
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true} 
   ],
   bootstrap: [AppComponent]
 })
